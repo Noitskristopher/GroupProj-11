4ef003a33814a4c727ca574a465775bed100e35c
Kristopher Cabado <cabadokris32@gmail.com>
2023-03-22 11:07:37 -0700

    added app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome Group 11!</h1>
+      <p>Testing new branch</p>
     </div>
   );
 }
