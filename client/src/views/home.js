7ac67aac1f061484de51daf6e0133bd592b925b4
HHarlow <111320408+HHarlow@users.noreply.github.com>
2023-03-26 13:09:16 -0600

    home form

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,14 +1,13 @@
 import React, {useState} from 'react'
 import axios from 'axios'
 import {link} from 'react-router-dom'
-import Nav from '../components/Nav'
 
-const Home = () => {
-    const [painLocation, setPainLocation] = useState("")
-    const [painDuration, setPainDuration] = useState("")
-    const [painLevel, setPainLevel] = useState("")
-    const [currentMeds, setCurrentMeds] = useState("")
-    const [appointmentDate, setAppointmentDate] = useState("")
+const Home = (props) => {
+    const [painLocation, setPainLocation] = useState("");
+    const [painDuration, setPainDuration] = useState("");
+    const [painLevel, setPainLevel] = useState("");
+    const [currentMeds, setCurrentMeds] = useState("");
+    const [appointmentDate, setAppointmentDate] = useState("");
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -18,16 +17,16 @@ const Home = () => {
             painDuration,
             painLevel,
             currentMeds,
-            appointmentDate
+            appointmentDate,
         })
         .then((res) => {
-            console.log(res)
-            console.log(res.data)
-            setPainLocation(""),
-            setPainDuration(""),
-            setPainLevel(""),
-            setCurrentMeds(""),
-            setAppointmentDate("")
+            console.log(res);
+            console.log(res.data);
+            setPainLocation("");
+            setPainDuration("");
+            setPainLevel("");
+            setCurrentMeds("");
+            setAppointmentDate("");
         })
         .catch((err)=>{
             console.log(err)
@@ -36,38 +35,48 @@ const Home = () => {
 
     return (
     <div>
-        <div>
-            <Nav />
-        </div>
         <form onSubmit={submitHandler}>
             <div>
                 <label>What's hurting?</label>
-                <input onChange={(e) = setPainLocation(e.target.value)}
-                value={painLocation}
-                type="text"></input>
+                <div>
+                    <input 
+                    onChange={(e) => setPainLocation(e.target.value)}
+                    value={painLocation}
+                    type="text"/>
+                </div>
             </div>
             <div>
                 <label>How long has it been hurting?</label>
-                <input onChange={(e) = setPainLocation(e.target.value)}
-                value={painDuration}
-                type="text"></input>
-            </div>    <div>
+                <div>
+                    <input onChange={(e) => setPainDuration(e.target.value)}
+                    value={painDuration}
+                    type="text"></input>
+                </div>
+            </div>    
+            <div>
                 <label>Pain level?</label>
-                <input onChange={(e) = setPainLocation(e.target.value)}
-                value={painLevel}
-                type="text"></input>
+                <div>
+                    <input onChange={(e) => setPainLevel(e.target.value)}
+                    value={painLevel}
+                    type="number"></input>
+                </div>
             </div>    <div>
                 <label>Any current medications?</label>
-                <input onChange={(e) = setPainLocation(e.target.value)}
-                value={currentMeds}
-                type="text"></input>
+                <div>
+                    <input onChange={(e) => setCurrentMeds(e.target.value)}
+                    value={currentMeds}
+                    type="text"></input>
+                </div>
             </div>
             <div>
                 <label>Appointment date</label>
-                <input onChange={(e) = setPainLocation(e.target.value)}
-                value={appointmentDate}
-                type="date"></input>
+                <div>
+                    <input onChange={(e) => setAppointmentDate(e.target.value)}
+                    value={appointmentDate}
+                    type="date"></input>
+                </div>
             </div>
+            <button image='submit'> Submit</button>
         </form>
     </div>
     )
