f7065bfcba6bcf6040af0993129d3e88f455515c
GunnarWalsh <104541337+GunnarWalsh@users.noreply.github.com>
2023-03-26 18:40:24 -0400

    Added back-end validation

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-import {link} from 'react-router-dom'
+import { link } from 'react-router-dom'
 
 const Home = (props) => {
     const [painLocation, setPainLocation] = useState("");
@@ -8,77 +8,103 @@ const Home = (props) => {
     const [painLevel, setPainLevel] = useState("");
     const [currentMeds, setCurrentMeds] = useState("");
     const [appointmentDate, setAppointmentDate] = useState("");
+    const [errors, setErrors] = useState({})
 
     const submitHandler = (e) => {
         e.preventDefault();
-        
-        axios.post("http://localhost:8000/api/ticket/new",{
+
+        axios.post("http://localhost:8000/api/ticket/new", {
             painLocation,
             painDuration,
             painLevel,
             currentMeds,
             appointmentDate,
         })
-        .then((res) => {
-            console.log(res);
-            console.log(res.data);
-            setPainLocation("");
-            setPainDuration("");
-            setPainLevel("");
-            setCurrentMeds("");
-            setAppointmentDate("");
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-}
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                setPainLocation("");
+                setPainDuration("");
+                setPainLevel("");
+                setCurrentMeds("");
+                setAppointmentDate("");
+            })
+            .catch((err) => {
+                console.log(err)
+                if (err.response.data.error) {
+                    setErrors(err.response.data.error.errors)
+                    console.log('^^Errors^^', errors)
+                }
+            })
+    }
 
     return (
-    <div>
-        <form onSubmit={submitHandler}>
-            <div>
-                <label>What's hurting?</label>
+        <div>
+            <form onSubmit={submitHandler}>
                 <div>
-                    <input 
-                    onChange={(e) => setPainLocation(e.target.value)}
-                    value={painLocation}
-                    type="text"/>
+                    <label>What's hurting?</label>
+                    <div>
+                        <input
+                            onChange={(e) => setPainLocation(e.target.value)}
+                            value={painLocation}
+                            type="text" />
+                        {
+                            errors.painLocation ?
+                                <p className='text-danger'>{errors.painLocation.message}</p> :
+                                null
+                        }
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label>How long has it been hurting?</label>
                 <div>
-                    <input onChange={(e) => setPainDuration(e.target.value)}
-                    value={painDuration}
-                    type="text"></input>
+                    <label>How long has it been hurting?</label>
+                    <div>
+                        <input onChange={(e) => setPainDuration(e.target.value)}
+                            value={painDuration}
+                            type="text" />
+                        {
+                            errors.painDuration ?
+                                <p className='text-danger'>{errors.painDuration.message}</p> :
+                                null
+                        }
+                    </div>
                 </div>
-            </div>    
-            <div>
-                <label>Pain level?</label>
                 <div>
-                    <input onChange={(e) => setPainLevel(e.target.value)}
-                    value={painLevel}
-                    type="number"></input>
+                    <label>Pain level?</label>
+                    <div>
+                        <input onChange={(e) => setPainLevel(e.target.value)}
+                            value={painLevel}
+                            type="number" />
+                        {
+                            errors.painLevel ?
+                                <p className='text-danger'>{errors.painLevel.message}</p> :
+                                null
+                        }
+                    </div>
                 </div>
-            </div>    <div>
-                <label>Any current medications?</label>
                 <div>
-                    <input onChange={(e) => setCurrentMeds(e.target.value)}
-                    value={currentMeds}
-                    type="text"></input>
+                    <label>Any current medications?</label>
+                    <div>
+                        <input onChange={(e) => setCurrentMeds(e.target.value)}
+                            value={currentMeds}
+                            type="text" />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label>Appointment date</label>
                 <div>
-                    <input onChange={(e) => setAppointmentDate(e.target.value)}
-                    value={appointmentDate}
-                    type="date"></input>
+                    <label>Appointment date</label>
+                    <div>
+                        <input onChange={(e) => setAppointmentDate(e.target.value)}
+                            value={appointmentDate}
+                            type="date" />
+                        {
+                            errors.appointmentDate ?
+                                <p className='text-danger'>{errors.appointmentDate.message}</p> :
+                                null
+                        }
+                    </div>
                 </div>
-            </div>
-            <button image='submit'> Submit</button>
-        </form>
-    </div>
+                <button image='submit'> Submit</button>
+            </form>
+        </div>
     )
 }
 
