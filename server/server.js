6af933145c31679781e0951e8903b2f0f12ca592
EJPhoenix <111155837+EJPhoenix@users.noreply.github.com>
2023-03-29 16:00:01 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -3,20 +3,20 @@ const app = express();
 const port = 8000;
 const cors = require('cors')
 // const socket = require('socket.io')
-// const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 
 require("./config/mongoose.config");
 // require("dotenv").config();
 
-// app.use(cookieParser())
+app.use(cookieParser())
 app.use(cors({credentials:true, origin:'http://localhost:3000'}))
 app.use(express.json(), express.urlencoded({ extended: true }));
 
-// const User = require('./models/user.model')
+const User = require('./models/user.model')
 const Symptoms= require('./models/PatientSymptoms')
-// const userRoutes = require("./routes/user.routes");
+const userRoutes = require("./routes/user.routes");
 const symptomsRoutes = require('./routes/symptoms.routes');
-// userRoutes(app);
+userRoutes(app);
 symptomsRoutes(app);
 
 const server = app.listen(port, () => console.log(`Listening on port: ${port}`) );
